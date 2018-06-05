b0f388cb5985fee67935b55242256d5dc98dfb57
lpennec <myskyprods@gmail.com>
2018-06-05 13:07:23 +0200

    second commit

---
+++ 2.MarketSigndown/Code/Market/Underwriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Market
+{
+    public class Underwriter
+    {
+
+        public int SeqNum { get; set; }
+        public string Name { get; set; }
+        public float WrittenLine { get; set; }
+        public float MinLine { get; set; }
+        public float SignedLine { get; set; }
+
+        public Underwriter(int seqNum, string name, float writtenLine, float minLine, float signedLine)
+        {
+            SeqNum = seqNum;
+            Name = name;
+            WrittenLine = writtenLine;
+            MinLine = minLine;
+            SignedLine = signedLine;
+        }
+    }
+}
